73730e27686d7aa948adbb26271f9e11b6ad2b2a
Siva837453 <sivaallaka777@gmail.com>
2024-05-22 07:42:32 +0530

    terraform

---
+++ count/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "db" {
+    # count = 3
+    count = length(var.instance_name)
+    ami = "ami-090252cbe067a9e58"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id]
+
+    tags = {
+
+        name = var.instance_name[count.index]
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name        = "allow_ssh"
+    description = "allowing ssh access"
+
+    ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"  
+    cidr_blocks      = ["0.0.0.0/0"]
+
+    }
+
+    
+    egress {
+    from_port        = 0  #from 0 to 0 means opening all ports
+    to_port          = 0
+    protocol         = "-1" #allow everything
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+    }
+
+
+    tags ={ 
+        name = "allow_ssh"
+        createdBy = "siva"
+    }
+}
